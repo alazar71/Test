5ff57ecc4bf72a2e8a248383fa784f7783feffef
alazar <adina.lazar@endava.com>
2024-06-13 08:27:53 +0300

    correct the warnings in master

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.sql.SQLOutput;
+//import java.sql.SQLOutput;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -11,8 +11,8 @@ public class Main {
         for (int i = 1; i <= 9; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            //System.out.println("i = " + i);
         }
-        System.out.println("this is a change for new branch newtest");
+        System.out.println("this is a change in master");
     }
 }
